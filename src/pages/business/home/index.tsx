eaa8578a473867401676ef7c4011f57041ec754a
Prince <princekizhthara@gmail.com>
2024-11-16 21:06:21 +0530

    feat(18): implement contact component

--- src/pages/business/home/index.tsx
+++ src/pages/business/home/index.tsx
@@ -7,24 +7,29 @@ import { mockCarousel1 } from 'src/mock/fullSizeBanner'
 
 import classes from './home.module.css'
 import { tile1MockData, tile2MockData } from 'src/mock/tile'
+import { Contact1 } from 'src/components/contact'
+import { mockContact } from 'src/mock/contact'
 
 export const Home: React.FC = () => {
   return (
     <div className={classes.watchTemplate}>
-      <div>
+      <div className={classes.componentContainer}>
         <Carousel1 images={mockCarousel1} />
       </div>
-      <div>
+      <div className={classes.componentContainer}>
         <Category1 />
       </div>
-      <div>
+      <div className={classes.componentContainer}>
         <ProductTile />
       </div>
-      <div>
+      <div className={classes.componentContainer}>
         <Tile1 {...tile1MockData} />
       </div>
-      <div>
+      <div className={classes.componentContainer}>
         <Tile2 {...tile2MockData} />
+      </div>
+      <div>
+        <Contact1 {...mockContact} />
       </div>
     </div>
   )
